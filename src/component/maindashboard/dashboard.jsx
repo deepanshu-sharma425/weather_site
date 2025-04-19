e9825450f09f4cacb4c095d47584fe0ff874a59b
Deepanshu sharma <chuiwaldeepanshu@gmail.com>
2025-04-19 23:14:12 +0530

    added images

--- src/component/maindashboard/dashboard.jsx
+++ src/component/maindashboard/dashboard.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
-function Dashboard() {
+function Dashboard({city}) {
+  const[weather,setweather]=useState(null)
+  const apiKey = 'b1d0579d30c58c38b4ab1543c5044ebe';
+  useEffect(()=>{
+    if(city='')return;
+    const datafetching=async()=>{
+      try{
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        const data=await response.json()
+        console.log(data)
+        setweather(data)
+    }
+    catch(err){
+      console.error("problem while fetching api")
+    }
+
+    }
+    datafetching()
+  },[city])
+  function Nextday(props){
+    return(
+      <>
+      <div className="nextday">
+        <div className="nextdayimgday">
+          <img src="" alt="" />
+          <h6>{props.day}</h6>
+          <p>{props.weathertype}</p>
+        </div>
+        <h3>{props.nextdaytemp}</h3>
+      </div>
+      </>
+    )
+  }
+
+    
+
+
   return (
-    <div>Dashboard</div>
+    <>
+    <div className="tempdisplayinfo">
+      <div className="tempinfo">
+        <img src="" alt="" />
+        <div className="locoinfotemp">
+          <div className="lococloudy">
+            <p>uk,uk</p>
+            <p>party cloudy</p>
+          </div>
+          <div className="tempdisplay">28.c</div>
+          
+
+        </div>
+      </div>
+    </div>
+    <Nextday/>
+
+
+    
+    </>
   )
 }
 
